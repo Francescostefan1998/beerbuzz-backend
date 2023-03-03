0a6c4847e5a26d66901429b7a97431276b9f9fba
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-03-03 15:00:24 +0100

    installin packages and initializing the server page

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import listEndpoints from "express-list-endpoints";
+import cors from "cors";
+import mongoose from "mongoose";
+import passport from "passport";
+
+const server = express();
+
+const port = process.env.PORT || 3001;
+
+const mongoConnectionString = process.env.MONGO_CONNECTION_STRING;
+
+server.use(cors());
+server.use(express.json());
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING);
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to Mongo");
+  server.listen(port, () => {
+    console.table(listEndpoints(server));
+    console.log(`Server running on Port: ${port}`);
+  });
+});
