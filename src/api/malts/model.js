f55670aa80980bd322c367206bc2a5935e225faa
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-03-04 11:29:40 +0100

    editing modal

--- src/api/malts/model.js
+++ src/api/malts/model.js
@@ -6,7 +6,7 @@ const maltSchema = new Schema(
     name: { type: String, required: true },
     supplier: { type: String },
     type: { type: String, required: true },
-    color: { type: String, required: true },
+    color: { type: Number, required: true },
     category: { type: String, required: true },
     origin: { type: String, required: true },
     potential: { type: Number, required: true },
--- src/api/yeasts/model.js
+++ src/api/yeasts/model.js
@@ -5,11 +5,14 @@ const yeastSchema = new Schema(
   {
     name: { type: String, required: true },
     techName: { type: String, required: true },
-    laboratory: { type: Number, required: true },
-    cellsPerGram: { type: String, required: true },
+    laboratory: { type: String, required: true },
+    cellsPerGram: { type: Number, required: true },
     type: { type: String, required: true },
     attenuation: { type: Number, required: true },
     form: { type: String, required: true },
+    maxAbv: { type: Number, required: true },
+    flocculation: { type: String, required: true },
+    temperature: [{ type: Number }],
   },
   {
     timestamps: true,
